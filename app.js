39ead8833c3d2b0ef08ecf805f9dc4de5991ee5a
charles <crramiress@gmail.com>
2021-06-01 17:29:04 -0300

    Now is the final final  project

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const express = require('express')
+const express = require("express")
 const app = express()
 const port = process.env.Port || 3000
 
-const routes = require('./routes')
+const routes = require("./routes")
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get("/", (req, res) => {
+    res.send("Hello World!")
 })
 
 app.use(express.json());
